a36bba774c315ed2b2d65dbc5dac8c6cc0102fc3
Farrel Rajwa <farrelrajwa04@gmail.com>
2026-02-03 23:46:05 +0700

    1423

--- lib/widget/report/report.dart
+++ lib/widget/report/report.dart
@@ -12,3 +12,14 @@ class ReportPage extends StatelessWidget {
     );
   }
 }
+
+class ReportSales extends StatelessWidget{
+  const ReportSales({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('testing'),),
+    );
+  }
+}
