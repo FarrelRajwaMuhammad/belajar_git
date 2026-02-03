5371264893acef6493b0ebe94229075a42cb2387
Farrel Rajwa <farrelrajwa04@gmail.com>
2026-02-03 23:42:30 +0700

    123

--- lib/widget/report/report.dart
+++ lib/widget/report/report.dart
@@ -7,7 +7,7 @@ class ReportPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Laporan"), backgroundColor: Colors.blue),
+      appBar: AppBar(title: const Text("Laporan"), backgroundColor: Colors.red),
       body: const Center(child: Text("Halaman Reportabcdefg")),
     );
   }
